6e9702eb9f7698256310e61f67e4cb988cbbd395
Feng Shijie <Shijie.Feng@amd.com>
2026-01-27 18:06:21 +0000

    Add utility nbmodules

---
+++ lib/Bindings/Python/FlyROCDLExtension.cpp
@@ -0,0 +1,43 @@
+#include "mlir-c/Bindings/Python/Interop.h"
+#include "mlir-c/Dialect/LLVM.h"
+#include "mlir-c/IR.h"
+#include "mlir-c/Support.h"
+#include "mlir/Bindings/Python/Nanobind.h"
+#include "mlir/Bindings/Python/NanobindAdaptors.h"
+#include "mlir/CAPI/IR.h"
+#include "mlir/CAPI/Wrap.h"
+
+#include <mlir/IR/BuiltinAttributes.h>
+#include <mlir/IR/MLIRContext.h>
+#include <mlir/IR/Value.h>
+
+#include "flydsl-c/FlyROCDLDialect.h"
+#include "flydsl/Dialect/Fly/IR/FlyDialect.h"
+#include "flydsl/Dialect/FlyROCDL/IR/Dialect.h"
+
+namespace nb = nanobind;
+using namespace nb::literals;
+using namespace mlir;
+using namespace mlir::fly;
+using namespace mlir::fly_rocdl;
+using namespace mlir::python::nanobind_adaptors;
+
+NB_MODULE(_fly_rocdl, m) {
+  m.doc() = "MLIR Python FlyROCDL Extension";
+
+  mlir_type_subclass(m, "MmaAtomCDNA3_MFMAType", mlirTypeIsAFlyROCDLMmaAtomCDNA3_MFMAType,
+                     mlirFlyROCDLMmaAtomCDNA3_MFMATypeGetTypeID)
+      .def_classmethod(
+          "get",
+          [](const nb::object &cls, int32_t m, int32_t n, int32_t k, MlirType elemTyA,
+             MlirType elemTyB, MlirType elemTyAcc, MlirContext context) {
+            return cls(wrap(MmaAtomCDNA3_MFMAType::get(m, n, k, unwrap(elemTyA), unwrap(elemTyB),
+                                                       unwrap(elemTyAcc))));
+          },
+          "cls"_a, "m"_a, "n"_a, "k"_a, "elem_ty_a"_a, "elem_ty_b"_a, "elem_ty_acc"_a,
+          "context"_a = nb::none(),
+          // clang-format off
+          nb::sig("def get(cls, m: int, n: int, k: int, elem_ty_a: " MAKE_MLIR_PYTHON_QUALNAME("ir.Type") ", elem_ty_b: " MAKE_MLIR_PYTHON_QUALNAME("ir.Type") ", elem_ty_acc: " MAKE_MLIR_PYTHON_QUALNAME("ir.Type") ", context: " MAKE_MLIR_PYTHON_QUALNAME("ir.Context") " | None = None) -> MmaAtomCDNA3_MFMAType"),
+          // clang-format on
+          "Create a MmaAtomCDNA3_MFMAType with m, n, k dimensions and element types");
+}
