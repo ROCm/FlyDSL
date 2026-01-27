1d6ddb8ae80ff29e7c83cd6f48e55d0fdc45ce2a
Feng Shijie <Shijie.Feng@amd.com>
2026-01-27 18:06:21 +0000

    Add utility nbmodules

--- lib/CAPI/Dialect/FlyROCDL/FlyROCDLDialect.cpp
+++ lib/CAPI/Dialect/FlyROCDL/FlyROCDLDialect.cpp
@@ -1,6 +1,22 @@
 #include "flydsl-c/FlyROCDLDialect.h"
 
 #include "flydsl/Dialect/FlyROCDL/IR/Dialect.h"
+#include "mlir/CAPI/IR.h"
 #include "mlir/CAPI/Registration.h"
 
+using namespace mlir;
+using namespace mlir::fly_rocdl;
+
 MLIR_DEFINE_CAPI_DIALECT_REGISTRATION(FlyROCDL, fly_rocdl, mlir::fly_rocdl::FlyROCDLDialect)
+
+//===----------------------------------------------------------------------===//
+// MmaAtomCDNA3_MFMAType
+//===----------------------------------------------------------------------===//
+
+bool mlirTypeIsAFlyROCDLMmaAtomCDNA3_MFMAType(MlirType type) {
+  return isa<MmaAtomCDNA3_MFMAType>(unwrap(type));
+}
+
+MlirTypeID mlirFlyROCDLMmaAtomCDNA3_MFMATypeGetTypeID(void) {
+  return wrap(MmaAtomCDNA3_MFMAType::getTypeID());
+}
