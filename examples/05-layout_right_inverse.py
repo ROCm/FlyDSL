6b7a7244e711fdc6162b505f0d0bc1baa52b0599
xudoyuan <xudoyuan@amd.com>
2026-02-09 11:32:23 +0000

    [FLYDSL]:Python refactoring and adaptation

--- examples/05-layout_right_inverse.py
+++ examples/05-layout_right_inverse.py
@@ -1,32 +1,18 @@
-import flydsl
-from flydsl import lang as fx
+import flydsl.compiler as fmc
+import flydsl.expr as fx
 
 
-class RightInverseDemo(fx.MlirModule):
-    def __init__(self):
-        super().__init__()
-
-    @fx.kernel
-    def kernel(self: fx.T.i64()):
-        layout = fx.make_layout((4, 2, 3), (3, 12, 1))
-        inv = fx.right_inverse(layout)
-        fx.print_op("right_inverse: {}", inv)
-
-    @fx.jit
-    def __call__(self: fx.T.i64()):
-        x = fx.arith.constant(fx.T.i64(), 1)
-        c1 = fx.arith.constant(fx.T.index(), 1)
-        kernel_sym = fx.ir.SymbolRefAttr.get(["kernels", "kernel"])
-        fx.LaunchFuncOp(
-            kernel_sym,
-            grid_size=[c1, c1, c1],
-            block_size=[c1, c1, c1],
-            kernel_operands=[x],
-        )
+@fmc.kernel
+def rightInverseKernel():
+    layout = fx.make_layout((4, 2, 3), (3, 12, 1))
+    inv = fx.right_inverse(layout)
+    fx.printf("before right_inverse: {}", layout)
+    fx.printf("after right_inverse: {}", inv)
 
 
-RightInverseDemo_Module = RightInverseDemo()
-print(RightInverseDemo_Module)
+@fmc.jit
+def runRightInverse(stream: fx.Stream = fx.Stream(None)):
+    rightInverseKernel().launch(grid=(1, 1, 1), block=[1, 1, 1], stream=stream.value)
 
-# Run pass pipeline and print IR after each pass.
-RightInverseDemo_Executor = flydsl.compile(RightInverseDemo_Module, print_after_all=False)
+
+runRightInverse()
