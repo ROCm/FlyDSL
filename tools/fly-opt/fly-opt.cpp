39cc503232e9247799f52c0b0e28581467d02d71
jli10004 <jli10004@amd.com>
2026-02-27 02:26:02 +0000

    Apply clang-format to fly-opt.cpp

--- tools/fly-opt/fly-opt.cpp
+++ tools/fly-opt/fly-opt.cpp
@@ -7,10 +7,10 @@
 #include "mlir/Support/FileUtilities.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 
+#include "flydsl/Conversion/Passes.h"
 #include "flydsl/Dialect/Fly/IR/FlyDialect.h"
 #include "flydsl/Dialect/Fly/Transforms/Passes.h"
 #include "flydsl/Dialect/FlyROCDL/IR/Dialect.h"
-#include "flydsl/Conversion/Passes.h"
 
 int main(int argc, char **argv) {
   mlir::registerAllPasses();
@@ -22,6 +22,5 @@ int main(int argc, char **argv) {
   registry.insert<mlir::fly::FlyDialect>();
   registry.insert<mlir::fly_rocdl::FlyROCDLDialect>();
 
-  return mlir::asMainReturnCode(
-      mlir::MlirOptMain(argc, argv, "Fly Optimizer Driver\n", registry));
+  return mlir::asMainReturnCode(mlir::MlirOptMain(argc, argv, "Fly Optimizer Driver\n", registry));
 }
