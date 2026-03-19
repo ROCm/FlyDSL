dfe9202db5fcff9a013edfffd7bffecd25fe717d
jli10004 <jli10004@amd.com>
2026-03-19 09:47:47 +0000

    remove local build scripts, ir_dump, and _mlir symlink from repo

--- env.sh
+++
@@ -1,5 +0,0 @@
-export MLIR_PATH=/data/jli/flydsl-ws/llvm-project/build-flydsl/mlir_install
-export PYTHONPATH=/data/jli/flydsl-ws/FlyDSL/.flir/build/python_packages/flydsl:/data/jli/flydsl-ws/FlyDSL/flydsl/src:/data/jli/flydsl-ws/FlyDSL:$PYTHONPATH
-export PATH=/data/jli/flydsl-ws/FlyDSL/.flir/build/bin:$PATH
-export SHOW_IR=1
-export PATH=/data/jli/flydsl-ws/llvm-project/build-flydsl/mlir_install/bin:$PATH
