0c08ffacfd6076d1ae744f8e273a12608453206b
yadaish <yadai@amd.com>
2026-04-16 05:36:06 +0000

    update

--- test_hgemm.sh
+++ test_hgemm.sh
@@ -4,7 +4,8 @@
 # Created Time: Mon Apr 13 09:14:34 2026
 #########################################################################
 source $GFX1250_MODEL_PATH/ffmlite_env.sh
+# source $GFX1250_MODEL_PATH/am_env.sh
 for SK in 1; do
   echo "===== split-k=${SK} ====="
-  EMU_MODE=1 FLYDSL_RUNTIME_ENABLE_CACHE=1 FLYDSL_DUMP_IR=1 PYTHONPATH=/workspace/ffm/FlyDSL/build-fly/python_packages:/workspace/ffm/FlyDSL python tests/kernels/test_hgemm_splitk_gfx1250.py -M 256 -N 8192 -K 1024 --split-k ${SK} --tile-m 128 --tile-n 128 --tile-k 64 --num-buffers 2 --m-warp 2 --n-warp 2 --num-iters 16 --num-warmup 3
+  EMU_MODE=0 FLYDSL_RUNTIME_ENABLE_CACHE=1 FLYDSL_DUMP_IR=1 PYTHONPATH=/workspace/ffm/FlyDSL/build-fly/python_packages:/workspace/ffm/FlyDSL python tests/kernels/test_hgemm_splitk_gfx1250.py -M 8192 -N 8192 -K 8192 --split-k ${SK} --tile-m 128 --tile-n 128 --tile-k 64 --num-buffers 2 --m-warp 2 --n-warp 2 --num-iters 2 --num-warmup 0
 done
