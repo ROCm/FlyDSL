d5b23cd38868f641605b7d8bc785324beb2857b7
Felix Li <felix.li@amd.com>
2026-04-18 20:46:14 +0800

    Update version

--- python/flydsl/__init__.py
+++ python/flydsl/__init__.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright (c) 2025 FlyDSL Project Contributors
 
-__version__ = _BASE_VERSION = "0.1.3.1"
+__version__ = _BASE_VERSION = "0.1.4"
 
 # FFM simulator compatibility shim (no-op outside simulator sessions).
 from ._compat import _maybe_preload_system_comgr  # noqa: E402
