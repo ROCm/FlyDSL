947d00b52617eef3648e5ec79bb7d85b84230616
Junlin Chen <junlchen@amd.com>
2026-06-17 13:22:34 +0800

    replace rsqrt API with fmath.rsqrt

--- kernels/layernorm_kernel.py
+++ kernels/layernorm_kernel.py
@@ -724,7 +724,7 @@ def _build_layernorm_quant_module(
             mean = sum_val / n_float
             var = sumsq_val / n_float - mean * mean
             var = (var < c_zero_f).select(c_zero_f, var)
-            rstd = (var + eps_c).rsqrt(fastmath=fm_fast)
+            rstd = fmath.rsqrt(var + eps_c, fastmath=fm_fast)
 
             thread_row_max = c_zero_f
             y_local = []
@@ -813,7 +813,7 @@ def _build_layernorm_quant_module(
             mean = sum_val / n_float
             var = sumsq_val / n_float - mean * mean
             var = (var < c_zero_f).select(c_zero_f, var)
-            rstd = (var + eps_c).rsqrt(fastmath=fm_fast)
+            rstd = fmath.rsqrt(var + eps_c, fastmath=fm_fast)
 
             thread_row_max = c_zero_f
             for base_idx_int in range_constexpr(0, N, BLOCK_THREADS):
@@ -1083,7 +1083,7 @@ def _build_fused_add_layernorm_quant_module(
             mean = sum_val / n_float
             var = sumsq_val / n_float - mean * mean
             var = (var < c_zero_f).select(c_zero_f, var)
-            rstd = (var + eps_c).rsqrt(fastmath=fm_fast)
+            rstd = fmath.rsqrt(var + eps_c, fastmath=fm_fast)
 
             thread_row_max = c_zero_f
             y_local = []
@@ -1184,7 +1184,7 @@ def _build_fused_add_layernorm_quant_module(
             mean = sum_val / n_float
             var = sumsq_val / n_float - mean * mean
             var = (var < c_zero_f).select(c_zero_f, var)
-            rstd = (var + eps_c).rsqrt(fastmath=fm_fast)
+            rstd = fmath.rsqrt(var + eps_c, fastmath=fm_fast)
 
             thread_row_max = c_zero_f
             for base_idx_int in range_constexpr(0, N, BLOCK_THREADS):
